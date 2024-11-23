fbff246042eed4e3cd5e02b115f77520c9699345
Santiago Morel <santi.morel@gmail.com>
2024-11-23 12:57:09 -0600

    reorganized code and add more projects

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#header").load("header.html");
+  $("#dawer").load("dawer.html");
+  $('#section_intro').load('section_intro.html');
+  $('#portfolio_sec').load('section_porfolio.html');
+  $('#interest_sec').load('section_interest.html');
+  $('#project_sec').load('section_project.html');
+  $('#contact_sec').load('section_contact.html');
+  $('#project_artemisa').load('project_artemisa.html');
+  $('#project_ardu').load('project_ardu.html');
+  $('#project_sorteador').load('project_sorteador.html');
+});
